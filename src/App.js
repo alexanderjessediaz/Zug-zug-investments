6296a3822c504e93f0e410e800ead6febcb30f59
AJ <alexanderjdiaz89@gmail.com>
2020-05-04 16:47:00 -0600

    add placeholder card and table to FE

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Navbar, Nav, Button } from 'react-bootstrap';
+import { Navbar, Nav, Button, Table } from 'react-bootstrap';
 
 import './App.css';
 
@@ -15,7 +15,62 @@ function App() {
                 </Nav>
         </Navbar>
       </header>
+
+      <div className="container" class="d-flex justify-content-center">
+        <div className="row mt=5">
+          <div className="col-1g-4 mb-4 grid-margin">
+            <div className="card h-100">
+              <h2 className="card-header">Realm: Kromcrush</h2>
+              <div className="card-body">
+                <p className="card-text">Auction House postings for Kromcrush</p>
+              </div>
+              <div className="card-footer" class="d-flex justify-content-center">
+                <Button variant="btn btn-primary">Click me</Button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
      
+     <div className="row mb-4">
+       <div className="col-sm-12 grid-margin">
+         <div className="card h-100">
+           <h4 className="card-header" class="d-flex justify-content-center">AH Table</h4>
+           <div className="card-body">
+             <Table striped bordered hover>
+              <thead>
+                <tr>
+                  <th>#</th>
+                  <th>First Name</th>
+                  <th>Last Name</th>
+                  <th>Username</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>1</td>
+                  <td>AJ</td>
+                  <td>Diaz</td>
+                  <td>zugzug</td>
+                </tr>
+                <tr>
+                  <td>2</td>
+                  <td>Jenn</td>
+                  <td>Grenier</td>
+                  <td>Jags8191</td>
+                </tr>
+                <tr>
+                  <td>3</td>
+                  <td>Jackie</td>
+                  <td>Grenier-diaz</td>
+                  <td>Jackiedog</td>
+                </tr>
+              </tbody>
+             </Table>
+           </div>
+         </div>
+       </div>
+     </div>
     </div>
   );
 }
