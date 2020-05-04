bb94a80e0d75ebeab5cc58a6cc17ccc284817482
AJ <alexanderjdiaz89@gmail.com>
2020-05-04 16:02:34 -0600

    add react bootstrap and basic navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import { Navbar, Nav, Button } from 'react-bootstrap';
 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <header>
+        <Navbar expand="1g" variant="dark" bg="dark">
+          <Navbar.Brand href="#">Black Lotus Market</Navbar.Brand>
+                <Nav className="mr-auto" class="d-flex justify-content-end">
+                  <Nav.Link href="#" >Sign Up</Nav.Link>
+                  <Nav.Link href="#" >Sign In</Nav.Link>
+                </Nav>
+        </Navbar>
+      </header>
      
     </div>
   );
