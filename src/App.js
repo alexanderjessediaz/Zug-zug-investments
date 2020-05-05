e817a563ec38d23798b37f339dea696c0fa89664
AJ <alexanderjdiaz89@gmail.com>
2020-05-04 19:13:28 -0600

    add nexushub node install

--- src/App.js
+++ src/App.js
@@ -1,78 +1,92 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Navbar, Nav, Button, Table } from 'react-bootstrap';
 
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header>
-        <Navbar expand="1g" variant="dark" bg="dark">
-          <Navbar.Brand href="#">Black Lotus Market</Navbar.Brand>
-                <Nav className="mr-auto" class="d-flex justify-content-end">
-                  <Nav.Link href="#" >Sign Up</Nav.Link>
-                  <Nav.Link href="#" >Sign In</Nav.Link>
-                </Nav>
-        </Navbar>
-      </header>
+class App extends Component {
 
-      <div className="container" class="d-flex justify-content-center">
-        <div className="row mt=5">
-          <div className="col-1g-4 mb-4 grid-margin">
-            <div className="card h-100">
-              <h2 className="card-header">Realm: Kromcrush</h2>
-              <div className="card-body">
-                <p className="card-text">Auction House postings for Kromcrush</p>
-              </div>
-              <div className="card-footer" class="d-flex justify-content-center">
-                <Button variant="btn btn-primary">Click me</Button>
+  state = {
+    realmData: []
+  }
+
+componentDidMount(){
+  fetch(`https://apiKey=XKM451xgfMyCk-1dnc26vFLGS9yDM6oe/item/{enus}/{kromcrush}`)
+    .then(response => response.json())
+    .then(kromcrushData => this.setState({realmData: console.log(kromcrushData)}))
+}
+
+  render () {
+    return (
+      <div className="App">
+        <header>
+          <Navbar expand="1g" variant="dark" bg="dark">
+            <Navbar.Brand href="#">Black Lotus Market</Navbar.Brand>
+                  <Nav className="mr-auto" class="d-flex justify-content-end">
+                    <Nav.Link href="#" >Sign Up</Nav.Link>
+                    <Nav.Link href="#" >Sign In</Nav.Link>
+                  </Nav>
+          </Navbar>
+        </header>
+  
+        <div className="container" class="d-flex justify-content-center">
+          <div className="row mt=5">
+            <div className="col-1g-4 mb-4 grid-margin">
+              <div className="card h-100">
+                <h2 className="card-header">Realm: Kromcrush</h2>
+                <div className="card-body">
+                  <p className="card-text">Auction House postings for Kromcrush</p>
+                </div>
+                <div className="card-footer" class="d-flex justify-content-center">
+                  <Button variant="btn btn-primary">Click me</Button>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-     
-     <div className="row mb-4">
-       <div className="col-sm-12 grid-margin">
-         <div className="card h-100">
-           <h4 className="card-header" class="d-flex justify-content-center">AH Table</h4>
-           <div className="card-body">
-             <Table striped bordered hover>
-              <thead>
-                <tr>
-                  <th>#</th>
-                  <th>First Name</th>
-                  <th>Last Name</th>
-                  <th>Username</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>1</td>
-                  <td>AJ</td>
-                  <td>Diaz</td>
-                  <td>zugzug</td>
-                </tr>
-                <tr>
-                  <td>2</td>
-                  <td>Jenn</td>
-                  <td>Grenier</td>
-                  <td>Jags8191</td>
-                </tr>
-                <tr>
-                  <td>3</td>
-                  <td>Jackie</td>
-                  <td>Grenier-diaz</td>
-                  <td>Jackiedog</td>
-                </tr>
-              </tbody>
-             </Table>
+       
+       <div className="row mb-4">
+         <div className="col-sm-12 grid-margin">
+           <div className="card h-100">
+             <h4 className="card-header" class="d-flex justify-content-center">AH Table</h4>
+             <div className="card-body">
+               <Table striped bordered hover>
+                <thead>
+                  <tr>
+                    <th>#</th>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Username</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>1</td>
+                    <td>AJ</td>
+                    <td>Diaz</td>
+                    <td>zugzug</td>
+                  </tr>
+                  <tr>
+                    <td>2</td>
+                    <td>Jenn</td>
+                    <td>Grenier</td>
+                    <td>Jags8191</td>
+                  </tr>
+                  <tr>
+                    <td>3</td>
+                    <td>Jackie</td>
+                    <td>Grenier-diaz</td>
+                    <td>Jackiedog</td>
+                  </tr>
+                </tbody>
+               </Table>
+             </div>
            </div>
          </div>
        </div>
-     </div>
-    </div>
-  );
-}
+      </div>
+    );
+  }
+
+  }
 
 export default App;
