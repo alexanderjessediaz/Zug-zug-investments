f5dc268ce6d12cd38b2da6351646b3872fb716b3
AJ <alexanderjdiaz89@gmail.com>
2020-05-06 11:15:41 -0600

    add jumbotron, remove card, edit table to black lotus

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Navbar, Nav, Button, Table } from 'react-bootstrap';
+import { Navbar, Nav, Table } from 'react-bootstrap';
 
 import './App.css';
 
@@ -24,7 +24,7 @@ class App extends Component {
       <div className="App">
         <header>
           <Navbar expand="1g" variant="dark" bg="dark">
-            <Navbar.Brand href="#">Black Lotus Market</Navbar.Brand>
+            <Navbar.Brand href="#">Zug-Zug Investments</Navbar.Brand>
                   <Nav className="mr-auto" class="d-flex justify-content-end">
                     <Nav.Link href="#" >Sign Up</Nav.Link>
                     <Nav.Link href="#" >Sign In</Nav.Link>
@@ -32,54 +32,36 @@ class App extends Component {
           </Navbar>
         </header>
   
-        <div className="container" class="d-flex justify-content-center">
-          <div className="row mt=5">
-            <div className="col-1g-4 mb-4 grid-margin">
-              <div className="card h-100">
-                <h2 className="card-header">Realm: Kromcrush</h2>
-                <div className="card-body">
-                  <p className="card-text">AH postings</p>
-                </div>
-                <div className="card-footer" class="d-flex justify-content-center">
-                  <Button variant="btn btn-primary">Click me</Button>
-                </div>
-              </div>
-            </div>
+        <div class="jumbotron jumbotron-fluid">
+          <div class="container">
+            <h1 class="display-4">Zug-Zug Investments</h1>
+            <p class="lead">The important World of Warcraft commodities</p>
+            <hr class="my-4">
+              
+            </hr>
           </div>
         </div>
        
        <div className="row mb-4">
          <div className="col-sm-12 grid-margin">
            <div className="card h-100">
-             <h4 className="card-header" class="d-flex justify-content-center">AH Table</h4>
+             <h4 className="card-header" class="d-flex justify-content-center">Kromcrush Prices</h4>
              <div className="card-body">
                <Table striped bordered hover nexus={this.state.nexus}>
                 <thead>
                   <tr>
-                    <th>#</th>
-                    <th>First Name</th>
-                    <th>Last Name</th>
-                    <th>Username</th>
+                    <th>Item Number</th>
+                    <th>Item Name</th>
+                    <th>Average Price</th>
+                    <th>Quantity</th>
                   </tr>
                 </thead>
                 <tbody>
                   <tr>
                     <td>1</td>
-                    <td>AJ</td>
-                    <td>Diaz</td>
-                    <td>zugzug</td>
-                  </tr>
-                  <tr>
-                    <td>2</td>
-                    <td>Jenn</td>
-                    <td>Grenier</td>
-                    <td>Jags8191</td>
-                  </tr>
-                  <tr>
-                    <td>3</td>
-                    <td>Jackie</td>
-                    <td>Grenier-diaz</td>
-                    <td>Jackiedog</td>
+                    <td>Black Lotus</td>
+                    <td>100g</td>
+                    <td>10</td>
                   </tr>
                 </tbody>
                </Table>
