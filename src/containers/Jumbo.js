6721af1c0e56bd12aed99d7949cd7b8468ebb34f
AJ <alexanderjdiaz89@gmail.com>
2020-05-07 09:55:56 -0600

    arrange image in Jumbo.js

--- src/containers/Jumbo.js
+++ src/containers/Jumbo.js
@@ -6,7 +6,17 @@ const Jumbo = () => {
         <Jumbotron fluid>
             <Container>
                 <h1>Zug-Zug Investments</h1>
-                <p>Only the most important World of Warcraft Commodities</p>
+                    <h6 class="d-flex justify-content-end">Only the most important World of Warcraft Commodities</h6>
+                <hr class="my-4"></hr>
+                <div class="media">
+                    <img 
+                        src="https://i.warosu.org/data/tg/img/0457/11/1456740861193.png?__cf_chl_jschl_tk__=845799104caa0759e627f7daf171d3185c85662b-1588805241-0-AXTuzQ3wJ1jkI8n69hcd9QHFxldxsI0gtNnJTLLSOJ_OpWjzlR4edFlr4GIM5TTKwGbwc2RaECZ0tKU3xy6xYWqQnFrXhO9bVpjhv_OtSAu7loaBpI85UyLTrUdagvl-xEtLD31e9_oSdHp7eacwrKieuRabc34htVOW_2SNlFBoZXRviiJcYQ5vSQCmhSn2LSC0JJ5WBMQ5lRvcTbNY0BLI4ulpI1MwJjXlWmkDts6vTbpo7D3rMD0vlmeck7nHWn5Pp3qpHmtWqt6vyE7Y5JINz-7Y8y0Mlg5faUXmgPBIetvyqiZ2W29qX2zvwRQFboQXjZ-GOzDg9dfLDHMbhJQ" 
+                        fluid 
+                        alt="orc in suit"
+                    />
+                    <div class="media-body">
+                    </div>
+                </div>
             </Container>
         </Jumbotron>
     )
--- src/containers/MainNavbar.js
+++ src/containers/MainNavbar.js
@@ -5,7 +5,15 @@ import { Navbar, Nav } from 'react-bootstrap';
     return (
         <header>
           <Navbar expand="1g" variant="dark" bg="dark">
-            <Navbar.Brand href="#">Zug-Zug Investments</Navbar.Brand>
+            <Navbar.Brand href="#">
+              <img 
+                src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fvignette1.wikia.nocookie.net%2Fwowwiki%2Fimages%2Fd%2Fd1%2FPeon_face_WC3.jpg%2Frevision%2Flatest%3Fcb%3D20081211004507&f=1&nofb=1"
+                width="60"
+                height="60"
+                alt="peon"
+              />
+              Zug-Zug
+            </Navbar.Brand>
                   <Nav className="mr-auto" class="d-flex justify-content-end">
                     <Nav.Link href="#" >Sign Up</Nav.Link>
                     <Nav.Link href="#" >Sign In</Nav.Link>
