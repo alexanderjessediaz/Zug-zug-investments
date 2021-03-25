89d4c836cf5e6644101dd1cdfff74cf8460e6d17
AJ <alexanderjessediaz@gmail.com>
2021-03-25 10:31:25 -0600

    remove Nav bar items with no content

--- src/Components/MainNavbar.js
+++ src/Components/MainNavbar.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Navbar, Nav } from 'react-bootstrap'
+import { Navbar} from 'react-bootstrap'
 import ZugZugLogo from '../Images/ZugZugLogo.png'
 
  const MainNavbar = () =>{
@@ -14,19 +14,7 @@ import ZugZugLogo from '../Images/ZugZugLogo.png'
                 alt="peon"
               />
             </Navbar.Brand>
-                  <Nav className="mr-auto">
-                    <Nav.Item>
-                      <Nav.Link href="http://localhost:3001/login">Login/Sign Up</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                      <Nav.Link href="http://loaclhost:3001/yourProfile">Your Profile</Nav.Link>
-                    </Nav.Item>
-                  </Nav>
-                  <Nav>
-                    <Nav.Link href="#">Advise</Nav.Link>
-                    <Nav.Link href="#">Kromarket</Nav.Link>
-                  </Nav>
-          </Navbar>
+            </Navbar>
         </header>
     )
 } 
