3612d10db61807f01722a13baf39f0c7ea597fe9
AJ <alexanderjessediaz@gmail.com>
2021-08-23 10:43:22 -0600

    change faction button to match other filter buttons

--- src/Components/Filters/FactionSelect.js
+++ src/Components/Filters/FactionSelect.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import { Dropdown } from 'react-bootstrap';
+import { Dropdown, FormControl } from 'react-bootstrap';
 
  const FactionSelect = ({ updateFactionString, setUserFaction }) => {
 
@@ -10,14 +10,69 @@ import { Dropdown } from 'react-bootstrap';
         setUserFaction(e);
     };
 
+    const FactionSelectToggle = React.forwardRef(({ children, onClick }, ref) => (
+        <a
+        id="server-toggle"
+        href="https://zugzug-inc.netlify.app/"
+        ref={ref}
+          onClick={(e) => {
+            e.preventDefault();
+              onClick(e);
+            }}
+            >
+            {children}
+            &#x25bc;
+        </a>
+      ));
+      
+    const FactionMenu = React.forwardRef(
+        ({ children, style, className, 'aria-labelledby': labeledBy }, ref) => {
+        const [value, setValue] = useState('');
+        return (
+          <div
+            ref={ref}
+            style={style}
+            className={className}
+            aria-labelledby={labeledBy}
+          >
+            <FormControl
+              autoFocus
+              className="mx-3 my-2 w-auto"
+              placeholder="Type to filter servers"
+              label="Type to filter servers"
+              onChange={(e) => setValue(factionNames)}
+              value={value}
+            />
+            <ul className="list-unstyled" id="dropdown-menu">
+              {React.Children.toArray(children).filter(
+                (child) =>
+                  !value || child.props.children.toLowerCase().startsWith(value),
+              )}
+            </ul>
+          </div>
+        );
+      },
+    );
+
+    const wowfactionNames = ["Alliance", "Horde"]
+
+    const factionNames = wowfactionNames.map( factionName => 
+        <Dropdown.Item
+            eventKey={factionName.toLowerCase()}
+            onSelect={handleSelect}
+        >{factionName}</Dropdown.Item>    
+    )
+    
+
     return (
         <Dropdown onSelect={handleSelect} >
-            <Dropdown.Toggle variant="warning" id="dropdown-basic">
+            <Dropdown.Toggle as={FactionSelectToggle} variant="warning" id="dropdown-basic">
                 {selectedFaction === '' ? "Choose Faction":selectedFaction}
             </Dropdown.Toggle>
-            <Dropdown.Menu >
-                <Dropdown.Item eventKey="horde">Horde</Dropdown.Item>
-                <Dropdown.Item eventKey="alliance" >Alliance</Dropdown.Item>
+            <Dropdown.Menu as={FactionMenu}>
+                {/* <Dropdown.Item eventKey="horde">Horde</Dropdown.Item>
+                <Dropdown.Item eventKey="alliance" >Alliance</Dropdown.Item> */}
+                {factionNames}
             </Dropdown.Menu>
         </Dropdown>
     );
